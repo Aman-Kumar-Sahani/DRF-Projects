197e10f75b85d37668417ba67bd0a0d6ce3b237a
unknown <Aman7548>
2020-12-24 20:14:55 +0530

    Initial Commit

---
+++ myapp.py
@@ -0,0 +1,14 @@
+import requests
+import json
+URL = "http://127.0.0.1:8000/stucreate/"
+data = {
+    'name':'Aman'
+    'roll':'1'
+    'city':'Gangtok'
+}
+#coneverting python data to json
+json_data = json.dumps(data)
+#sending data so post requests
+r = requests.post(url = URL,data =json_data)
+data = r.json()
+print(data)
